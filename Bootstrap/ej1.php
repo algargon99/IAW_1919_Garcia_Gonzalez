7c55cf0a0cb7df7882b6feff2e3c076f8af58022
algargon99 <a.gargon99@gmail.com>
2019-01-15 10:57:52 +0100

    15/01/2019

---
+++ Bootstrap/ej1.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="estilos.css">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</head>
+<body>
+  <div class="container">
+        <div class="jumbotron"><h1>TITULO</h1></div>
+        
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">Features</a>
+                <a class="nav-item nav-link" href="#">Pricing</a>
+                <a class="nav-item nav-link disabled" href="#">Disabled</a>
+                </div>
+            </div>
+            </nav>
+
+        <div class="row">
+            <div class="col-sm-6 col-md-8">
+                <img class="img-fluid" src="uno.jpeg">
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, quod! Corporis veniam nesciunt soluta dolore ad, optio esse adipisci ipsa, tempore sint amet ipsam quidem cum consequatur hic velit accusamus?</p>
+            </div >
+
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-sm-6 col-md-4">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, quod! Corporis veniam nesciunt soluta dolore ad, optio esse adipisci ipsa, tempore sint amet ipsam quidem cum consequatur hic velit accusamus?</p>
+            </div >
+            <div class="col-sm-6 col-md-4">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, quod! Corporis veniam nesciunt soluta dolore ad, optio esse adipisci ipsa, tempore sint amet ipsam quidem cum consequatur hic velit accusamus?</p>
+            </div >
+            <div class="col-sm-6 col-md-4">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, quod! Corporis veniam nesciunt soluta dolore ad, optio esse adipisci ipsa, tempore sint amet ipsam quidem cum consequatur hic velit accusamus?</p>
+            </div >
+        </div>
+  </div>
+</body>
+</html>
